86a45be1f7fd5bd2c76ce07195f4dcb785fdab41
Dezzz <deziahgayle@gmail.com>
2023-03-09 13:58:44 -0500

    Tested encryptCaesar function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include "caesar.h"
 #include <iostream>
+#include <string>
 
+int main() {
+  std::string plaintext = "Hello, World!";
+  int rshift = 3;
+  std::string ciphertext = encryptCaesar(plaintext, rshift);
+  std::cout << "Plaintext: " << plaintext << std::endl;
+  std::cout << "Ciphertext: " << ciphertext << std::endl;
 
-#include "funcs.h"
-
-int main()
-{
   return 0;
 }
