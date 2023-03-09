fe878e66bcede04765116d4289c26f35b48c430a
Dezzz <deziahgayle@gmail.com>
2023-03-09 14:00:32 -0500

    Added test_case for shiftChar function

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
 
 // add your tests here
+TEST_CASE("shiftChar") {
+    CHECK(shiftChar('a', 3) == 'd');
+    CHECK(shiftChar('A', 3) == 'D');
+    CHECK(shiftChar('z', 1) == 'a');
+    CHECK(shiftChar('Z', 1) == 'A');
+    CHECK(shiftChar(' ', 5) == ' ');
+}
