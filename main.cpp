0115c0d3d2bd7442c278c9bdc6e8c673f6acde16
Dezzz <deziahgayle@gmail.com>
2023-03-10 00:09:56 -0500

    Added test for vigenere function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "caesar.h"
+#include "vigenere.h"
 #include <iostream>
 #include <string>
 
-int main() {
-  std::string plaintext = "Hello, World!";
-  int rshift = 3;
-  std::string ciphertext = encryptCaesar(plaintext, rshift);
-  std::cout << "Plaintext: " << plaintext << std::endl;
-  std::cout << "Ciphertext: " << ciphertext << std::endl;
 
-  return 0;
+int main() {
+    std::string plaintext = "Hello, World!";
+    int rshift = 3;
+    std::string ciphertext = encryptCaesar(plaintext, rshift);
+    std::cout << "Plaintext: " << plaintext << std::endl;
+    std::cout << "Ciphertext: " << ciphertext << std::endl;
+    
+    std::string keyword = "cake";
+    std::string vin_ciphertext = encryptVigenere(plaintext, keyword);
+    std::cout << "Plaintext: " << plaintext << std::endl;
+    std::cout << "Ciphertext: " << vin_ciphertext << std::endl;
+
+
+    return 0;
 }
