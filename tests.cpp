8105c410ec2e31da10a0eba4ea7af553f8fece07
Deziah Gayle <81055368+Deziahh@users.noreply.github.com>
2023-03-10 03:13:48 -0500

    Added correct tests

--- tests.cpp
+++ tests.cpp
@@ -2,33 +2,37 @@
 #include "doctest.h"
 
 #include "caesar.h"
-#include "decryption.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 // add your tests here
 TEST_CASE("shiftChar") {
-    CHECK(shiftChar('a', 3) == 'd');
-    CHECK(shiftChar('A', 3) == 'D');
-    CHECK(shiftChar('z', 1) == 'a');
-    CHECK(shiftChar('Z', 1) == 'A');
-    CHECK(shiftChar(' ', 5) == ' ');
+  CHECK(shiftChar('a', 3) == 'd');
+  CHECK(shiftChar('A', 3) == 'D');
+  CHECK(shiftChar('z', 1) == 'a');
+  CHECK(shiftChar('Z', 1) == 'A');
+  CHECK(shiftChar(' ', 5) == ' ');
+}
+
+TEST_CASE("Caesar encryption") {
+  CHECK(encryptCaesar("Hello World", 3) == "Khoor Zruog");
+  CHECK(encryptCaesar("lemon", 3) == "ohprq");
+}
+
+TEST_CASE("Vingenere encryption") {
+  CHECK(encryptVigenere("lemon", "love") == "wshsy");
+  CHECK(encryptVigenere("Lemon", "apple") == "Rtbzr");
+  CHECK(encryptVigenere("Lemon", "cake") == "Tewsp");
 }
 
 TEST_CASE("Caesar cipher decryption") {
-    CHECK(decryptCaesar("Khoor zruog!", 3) == "Hello world!");
-    CHECK(decryptCaesar("Ebiil tloia!", 5) == "Hello world!");
+  CHECK(decryptCaesar("ohprq", 3) == "lemon");
+  CHECK(decryptCaesar("Ohprq", 3) == "Lemon");
 }
 
 TEST_CASE("Vigenere cipher decryption") {
-    CHECK(decryptVigenere("Lxfopvefrnhr", "lemon") == "lemonlemonle");
-    CHECK(decryptVigenere("Hvszyatwwat", "orange") == "orangeorange");
-}
-
-TEST_CASE("Decryption with empty strings") {
-    CHECK(decryptCaesar("", 3) == "");
-    CHECK(decryptVigenere("", "lemon") == "");
-}
-
-TEST_CASE("Decryption with invalid inputs") {
-    CHECK(decryptCaesar("Hello world!", -3) == "Khoor zruog!");
-    CHECK(decryptVigenere("Hello world!", "") == "Hello world!");
+  CHECK(decryptVigenere("wshsy", "love") == "lemon");
+  CHECK(decryptVigenere("newsp", "cake") == "lemon");
+  CHECK(decryptVigenere("Tewsp", "cake") == "Lemon");
+  
 }
